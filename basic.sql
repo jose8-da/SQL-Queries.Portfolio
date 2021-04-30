0b4be362a88c31682f52d557301dab63536528a7
José Luis Sanabria <83144145+jose8-da@users.noreply.github.com>
2021-04-30 04:11:14 +0100

    Create basic.sql

---
+++ basic.sql
@@ -0,0 +1,27 @@
+SQL queries  to find film title containing the word Uptowm in any position 
+SELECT film_id, title, description 
+FROM film
+WHERE title = '%Uptown%'
+
+-- Film length is more than 120 minutes and rental rate is more than 2.99 
+SELECT film_id, title, description 
+FROM film 
+WHERE length > 120 AND rental_rate > 2.99 
+ORDER BY film_id 
+
+-- Rental duration is between 3 and 7 days 
+SELECT film_id, title, description 
+FROM film 
+WHERE rental_duration BETWEEN 3 and 7 
+ORDER BY rental_duration, title 
+
+-- Film replacment cost is less than 14.99 
+SELECT film_id, title, description, replacement_cost 
+FROM film 
+WHERE replacement_cost DESC, title 
+
+-- Film rating is either PG or G 
+SELECT film_id, title, description, rating 
+FROM film 
+WHERE rating IN ('PG', 'G')
+ORDER BY rating, film_id 
